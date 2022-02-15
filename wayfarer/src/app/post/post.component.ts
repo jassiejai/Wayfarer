25b9b0034773ff97fd2e16e551a289d31cd2e6dc
Jassie <jasminehart94@gmail.com>
2022-02-15 14:13:31 -0800

    working on post component

--- wayfarer/src/app/post/post.component.ts
+++ wayfarer/src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CITIES } from '../cities';
+import { AppRoutingModule } from '../app-routing.module';
 
 @Component({
   selector: 'app-post',
@@ -8,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  cities : any = CITIES;
+  city: any;
   post : any; 
 
-  constructor() { }
+  constructor(private route: AppRoutingModule) { }
 
   ngOnInit(): void {
+    this.routes.paramMap.subscribe(param => {
+
+      this.city = city;
+      this.post = this.city.posts[postId];
+
+
+
+
+    })
   }
 
 }
