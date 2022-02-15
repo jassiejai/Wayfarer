c54a2016ecf0f1ac3f4dcd6ff8cf49a91568622b
Jassie <jasminehart94@gmail.com>
2022-02-15 12:50:15 -0800

    Pulling

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -7,19 +7,23 @@ import { CitiesComponent } from './cities/cities.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     CitiesComponent,
-    NavbarComponent
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
