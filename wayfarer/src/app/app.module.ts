fbaa8aa0a0d444a109358de68a280f187833bdb4
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-15 13:33:36 -0800

    fixed cities array, changed from js to ts file. Have a routing method in navbarcomponent.ts, still working on connecting it to the dropdown menu.

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { CitiesComponent } from './cities/cities.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
