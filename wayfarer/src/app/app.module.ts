936a48c055a852cbb7e9b7193c445d5eb2c552ed
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-15 09:43:10 -0800

    working on nav

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -4,8 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -15,8 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
