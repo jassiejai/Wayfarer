76ef7c91d2f12acbb02cca94537a81cc28865449
Jassie <jasminehart94@gmail.com>
2022-02-16 07:15:16 -0800

    commiting

--- wayfarer/src/app/app-routing.module.ts
+++ wayfarer/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CitiesComponent } from './cities/cities.component';
-// import { PostComponent } from './post/post.component';
+import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
   {path: 'cities', 
   component: CitiesComponent,
   // children : [
-  //   {
-  //     path: ':id/posts/:postId',
-  //     component: PostComponent
+  //   // {
+  //   //   path: ':id/posts/:postId',
+  //   //   component: PostComponent
 
-  //   }
+  //   // }
   // ]
 }
 ];
@@ -20,4 +20,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {}
--- wayfarer/src/app/post/post.component.ts
+++ wayfarer/src/app/post/post.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CITIES } from '../cities';
-import { AppRoutingModule } from '../app-routing.module';
 
 @Component({
   selector: 'app-post',
@@ -11,24 +10,23 @@ export class PostComponent implements OnInit {
 
   cities : any = CITIES;
   city: any;
-  post : any; 
+  post: any;
 
-  constructor(private route: AppRoutingModule) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    // this.route.param.subscribe(param => {
-
-    //   let postId = parseInt(param.get('post'), 15)
-
-    //   // this.city = CITIES.find(city =>{
-    //   //   return city.id == parseInt(param.get('id'), 15);
-    //   // });
-
-    //   this.post = this.city.posts[postId];
-    // });
-
-    // console.log(this.city, this.post)
-
-  }
-
+      // this.route.paramMap.subscribe(param => {
+  
+      //   const postId = parseInt(param.get('postId'), 15)
+  
+      //   this.city = CITIES.find(city =>{
+      //     return city.id === parseInt(param.get('id'), 15);
+      //   });
+  
+      //   this.post = this.city.posts[postId];
+      // });
+  
+      console.log(this.city, this.post)
+    }
 }
