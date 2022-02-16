8899d25e0e3c2c65be4a60f8974f787e7d13a019
Jassie <jasminehart94@gmail.com>
2022-02-16 07:57:43 -0800

    commiting

--- wayfarer/src/app/post/post.component.ts
+++ wayfarer/src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CITIES } from '../cities';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -12,21 +13,31 @@ export class PostComponent implements OnInit {
   city: any;
   post: any;
 
+  test : string ="miami";
 
-  constructor() { }
+
+  constructor( private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-      // this.route.paramMap.subscribe(param => {
+
+    this.route.paramMap.subscribe(params => {
+      this.city = CITIES.find(city => {
+        let paramId: string = params.get('id') || '';
+        return city.id === parseInt(paramId);})
+
+        console.log(this.city.name)
+      }) }
+    //   this.route.paramMap.subscribe(param => {
   
-      //   const postId = parseInt(param.get('postId'), 15)
+    //     // const postId = parseInt(param'postId')
   
-      //   this.city = CITIES.find(city =>{
-      //     return city.id === parseInt(param.get('id'), 15);
-      //   });
+    //     this.city = CITIES.find(city =>{
+    //       return city.id === parseInt(paramId);
+    //     });
   
-      //   this.post = this.city.posts[postId];
-      // });
+    //     this.post = this.city.posts[postId];
+    //   });
   
-      console.log(this.city, this.post)
-    }
+    //   console.log(this.city, this.post)
+    // }
 }
