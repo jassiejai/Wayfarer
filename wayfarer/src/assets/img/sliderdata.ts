d7ec3800cc825f27c1e562d3a1d108a0757f5350
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-16 07:07:48 -0800

    changed the cities.ts city id to start from 1 and changed the index to start at 1 to match. Now the first city will show up at /cities/1

---
+++ wayfarer/src/assets/img/sliderdata.ts
@@ -0,0 +1,9 @@
+export const sliderArray = [
+    {img: './gibraltar/gibraltar1-pexels-anna-urlapova-2968743.jpg', alt: '', text: '365 Days Of weddings a year'},
+    {img: './gibraltar/gibraltar2-pexels-martin-magnemyr-5101563.jpg', alt: '',  text: '365 Days Of weddings a year'},
+    {img: './london/london1-pexels-chris-schippers-427679.jpg', alt: '', text: '365 Days Of weddings a year'},
+    {img: './london/london2-pexels-pixabay-460672.jpg', alt: '',  text: '365 Days Of weddings a year'},
+    {img: './san-francisco/san-francisco1-pexels-pixabay-208745.jpg', alt: '', text: '365 Days Of weddings a year'},
+    {img: './san-francisco/san-francisco2-pexels-pixabay-258447.jpg', alt: '', text: '365 Days Of weddings a year'}
+   ];
+   
