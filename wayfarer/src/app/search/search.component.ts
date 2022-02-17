d7fdd7e8343e1e8094c3800955162aae0463692e
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-17 14:38:40 -0800

    Working search. Directs url as needed

--- wayfarer/src/app/search/search.component.ts
+++ wayfarer/src/app/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CITIES } from '../cities';
+// import { CITIES } from '../cities';
 import { ActivatedRoute, Router } from '@angular/router';
-import { POSTS } from '../posts'
+import { POSTS } from '../posts' // importing post array for the search
 
 @Component({
   selector: 'app-search',
@@ -9,18 +9,18 @@ import { POSTS } from '../posts'
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  cityList: any = CITIES;
+  // cityList: any = CITIES;
   postList: any = POSTS;
-  cityPosts: any = this.cityList.posts;
+  // cityPosts: any = this.cityList.posts;
   // matchingPosts: any = [];
-  cities: any;
+  // cities: any;
   posts: any;
 
-//TODO: Add search logic
+
     public searchText: string = '';
     // dataset = ['MDB', 'Angular', 'Bootstrap', 'Framework', 'SPA', 'React', 'Vue'];
 
-    constructor(private route: ActivatedRoute, private router: Router) { console.log(this.cities)}
+    constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     // this.findPost("title 1");
@@ -30,30 +30,25 @@ export class SearchComponent implements OnInit {
     //     return city.id === parseInt(paramId);
     //   });
     // })
-    this.route.paramMap.subscribe(params => {
-      this.posts = POSTS.find((post: { title: string | null; }) => {
-        let paramTitle: string = params.get('title') || '';
-        console.log(paramTitle);
-        return post.title === paramTitle;
-      });
-    })
+    // this.route.paramMap.subscribe(params => {
+    //   this.posts = POSTS.find((post: { title: string | null; }) => {
+    //     let paramTitle: string = params.get('title') || '';
+    //     console.log(paramTitle);
+    //     return post.title === paramTitle;
+    //   });
+    // })
   }
+  // Compares the innerHTML title to the postList title, if match is found then pull the city id and post id and route
   onChange(event: any){
-    console.log(event.originalTarget.innerHTML);
-    console.log(this.postList)
     for (let i = 0; i < this.postList.length; i++) {
       if (event.originalTarget.innerHTML==this.postList[i].title){
-        console.log("match");
-        console.log(this.postList[i].id);
-        console.log(this.postList[i].cityId);
-        console.log(this.postList[i].date);
         this.router.navigate(['cities/',this.postList[i].cityId,'post',this.postList[i].id]);
       }
     }
 
     // this.router.navigate(['cities/',event.value]);
   }
-
+  // Clears search bar after a selection is made
   clearSearch() {
     this.searchText = '';
   }
