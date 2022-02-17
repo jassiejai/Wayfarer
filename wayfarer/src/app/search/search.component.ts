c08f866293e008de3b6000506ca8730f351b3ec7
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-16 16:11:17 -0800

    Search working with local array. In process of making it work with an object.

--- wayfarer/src/app/search/search.component.ts
+++ wayfarer/src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { FilterPipe } from '../filter.pipe'
+import { CITIES } from '../cities';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+ cityList: any = CITIES;
  
-  city: string = '';
+  cities: any;
 //TODO: Add search logic 
     public searchText: string = '';
     dataset = ['MDB', 'Angular', 'Bootstrap', 'Framework', 'SPA', 'React', 'Vue'];
 
+    constructor(private route: ActivatedRoute, private router: Router) { console.log(this.cityList[0].name)}
+  
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.cities = CITIES.find( (city: { id: number; }) => {
+        let paramId: string = params.get('id') || '';
+        return city.id === parseInt(paramId);
+      }); 
+    })
   }
 
 }
