78be1a4f4c49dd289968357f504192f216b4b433
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-19 10:25:25 -0800

    Slider working, bootstrap install in project

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -6,23 +6,17 @@ import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-// import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostComponent } from './post/post.component';
 import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
 import { SliderComponent } from './slider/slider.component';
-// import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-
-// import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FilterPipe } from './filter.pipe';
 import { HttpClientModule } from '@angular/common/http';
-// import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel'; // For future use for front page carousel
-// import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 import { ReactiveFormsModule } from '@angular/forms';
-// import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 import { FormComponent } from './form/form.component';
+
 
 
 @NgModule({
@@ -45,10 +39,8 @@ import { FormComponent } from './form/form.component';
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule, 
-    // MdbCheckboxModule, 
-    ReactiveFormsModule
-// MDBBootstrapModule,
-// MdbCollapseModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
